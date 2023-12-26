266d13e31ad7d4e20030719fef72fa4866b43375
matoszl <pedrobr152015@hotmail.com>
2023-12-26 16:48:40 -0300

    Desafio concluííido!

--- Models/Smartphone.cs
+++ Models/Smartphone.cs
@@ -3,12 +3,16 @@ namespace DesafioPOO.Models
     public abstract class Smartphone
     {
         public string Numero { get; set; }
-        // TODO: Implementar as propriedades faltantes de acordo com o diagrama
+        private string Modelo { get; set; }
+        private string IMEI { get; set; }
+        private int Memoria { get; set; }
 
-        public Smartphone(string numero)
+        public Smartphone(string numero, string modelo, string imei, int memoria)
         {
             Numero = numero;
-            // TODO: Passar os parâmetros do construtor para as propriedades
+            Modelo = modelo;
+            IMEI = imei;
+            Memoria = memoria;
         }
 
         public void Ligar()
@@ -18,7 +22,17 @@ namespace DesafioPOO.Models
 
         public void ReceberLigacao()
         {
-            Console.WriteLine("Recebendo ligação...");
+            Console.WriteLine($"Recebendo ligação de 'Desconhecido'...");
+            Console.WriteLine("Deseja atender? ( 1 - sim | 2 - não) ");
+            int escolha = int.Parse(Console.ReadLine());
+            if (escolha == 1)
+            {
+                Console.WriteLine("'Sete dias...'\nTututu");
+            }
+            else
+            {
+                Console.WriteLine("Tututu");
+            }
         }
 
         public abstract void InstalarAplicativo(string nomeApp);
--- Program.cs
+++ Program.cs
@@ -1,3 +1,15 @@
 ﻿using DesafioPOO.Models;
 
-// TODO: Realizar os testes com as classes Nokia e Iphone
+Console.WriteLine("Iphone: ");
+Smartphone iphone = new Iphone("(11)93333-1111", "Iphone 5", "111111", 64);
+iphone.InstalarAplicativo("Whatsapp");
+iphone.ReceberLigacao();
+
+
+Console.ReadLine();
+Console.WriteLine("\n");
+
+Console.WriteLine("Nokia: ");
+Smartphone nokia = new Nokia("(11)91111-2222", "Tijolão", "222222", 128);
+nokia.Ligar();
+nokia.InstalarAplicativo("Youtube");
